a289c806560a2e3acc4b7dd98091b513eeb4bb98
dibashthapa <dibashthapa55@gmail.com>
2020-05-26 22:24:40 +0545

    Trash Files removed

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, Flask , jsonify, render_template, session, request, redirect
-from app.models import User
 auth_bp = Blueprint("auth_bp", __name__, template_folder = "templates/auth/")
-user = User()
+
 @auth_bp.route("/login", methods=["GET", "POST"])
 def main():
     if request.method == "POST":
@@ -15,7 +14,7 @@ def signup():
 		name = request.form['name']
 		email = request.form['email']
 		password = request.form['password']
-		user.add(name,email,password)
+		
 		return jsonify("success")
 	else:
 		return render_template("signup.html")
--- app/models.py
+++ app/models.py
@@ -1,16 +1,11 @@
 import sqlite3
-import traceback
-import os
-DATABASE= "site.db"
 
-def connect(db = DATABASE):
-    conn = sqlite3.connect(db)
-    return conn
+DATABASE= "site.db"
 
 class Database:
     def __init__(self):
-        conn= connect()
-        self.conn= conn
+        db= DATABASE
+        conn = sqlite3.connect(db, check_same_thread=False)
         self.cursor = conn.cursor()
         self.cursor.row_factory = sqlite3.Row
 
@@ -28,44 +23,6 @@ class Product(Database):
         the given table
         """
         db = self.db
-        try:
-            db.execute(f"SELECT * FROM {self.product_name}")
-            products = db.fetchall()
-            return products
-        except sqlite3.OperationalError:
-            return None
-
-
-class User(Database):
-    def __init__(self):
-        self.db = Database().get_cursor()
-        self.conn= Database().conn
-
-    def add(self):
-        db = self.db
-        conn = self.conn
-        try:
-            sql = "SELECT * FROM Users"
-            db.execute(sql)
-            print(db.fetchall())
-        except Exception as e:
-            print(e)
-
-    def exits(email):
-        db = self.db
-        db.execute(f"SELECT email FROM user WHERE user.email={email}")
-        user=db.fetchone()
-        return True if user == email else False
-
-    def verify(email, password):
-        db = self.db
-        db.execute(f"SELECT COUNT(email, password) FROM user WHERE user.email = {email} AND user.password = {password}")
-        rows = db.fetchone()
-        print(rows)
-
-
-
-if __name__=="__main__":
-    user = User()
-    user.add()
-
+        db.execute(f"SELECT * FROM {self.product_name}")
+        products = db.fetchall()
+        return products
