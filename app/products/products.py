e78cd765aab9427ad768e404824f75471ec908d0
dibashthapa <dibashthapa55@gmail.com>
2020-05-29 14:56:40 +0545

    New changes made

--- app/products/products.py
+++ app/products/products.py
@@ -1,17 +1,17 @@
 from flask import Flask , Blueprint , render_template, jsonify, request, abort
 from app.models import Product
 products_bp = Blueprint("products_bp", __name__, template_folder="templates/products")
+
+
 @products_bp.route("/<product>")
 def main(product):
-	start = int(request.args.get("start") or 0)
-    end = int(request.args.get("end") or len(product_items)-1)
 	product = Product(product)
 	product_items = product.return_items()
 	if product_items is None:
 		abort(404)
 	else:
 		product_items= [dict(p) for p in product_items]
-		return render_template("list.html", products = product_items)
+		return render_template("list.html")
 	
 
 @products_bp.route("/<product>/<product_item>")
