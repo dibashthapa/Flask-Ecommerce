670da84d75db62b6f921958f35e97fc793c566b5
dibashthapa <dibashthapa55@gmail.com>
2020-05-31 22:47:00 +0545

    Production site added

--- app/cart/cart.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-cart_bp = Blueprint("cart_bp", __name__, template_folder = "templates")
-
-@cart_bp.route("/view")
-def main():
-	return render_template("cart/view.html")
-
